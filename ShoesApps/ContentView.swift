b9a3d281642f97b67449e47477fd914243cbec22
Ahmad Maulana <ahmad.maulana@alodokter.com>
2020-08-30 22:00:06 +0700

    Initial Commit

---
+++ ShoesApps/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ShoesApps
+//
+//  Created by Ahmad Maulana on 30/08/20.
+//  Copyright © 2020 Ahmad Maulana. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
